b0e9a5f1158c6f9232e7a1284ff3a97e7ed31ce1
goinesjalen <goinesjalen@gmail.com>
2024-03-03 20:59:02 -0500

    Cleaning up logical errors in js

--- script/coffeeScript.js
+++ script/coffeeScript.js
@@ -125,46 +125,6 @@ async function getReceipt() {
         let message = "The following error occurred:\n" + error + "\n\nPlease try again or contact the customer support team";
         alert(message);
     }
-
-
-    let receipt = document.getElementById("getReceipt");
-    receipt.innerHTML = "";
-    let title = document.createElement("h2");
-    let titleVal = document.createTextNode("Order Placed:");
-    title.appendChild(titleVal);
-    receipt.appendChild(title);
-
-    // Grab order ID from Backend
-    let id = document.createElement("h2");
-    let idVal = document.createTextNode("Order id: ##WIP##");
-    id.appendChild(idVal);
-    receipt.appendChild(id);
-
-    let cond = document.createElement("h2");
-    let condiments = JSON.parse(localStorage.getItem("condiments"));
-    let condStr = localStorage.getItem("beverage") + " with ";
-    condStr = condStr.charAt(0).toUpperCase() + condStr.slice(1);
-    let iterationCount = 0;
-    for (let c of condiments) {
-        iterationCount++;
-        if (iterationCount == condiments.length){
-            condStr += " and " + c
-        }
-        else {
-            console.log(c.indexOf());
-            condStr += c + ", "
-        }
-    } 
-    let condVal = document.createTextNode(condStr + "!")
-    cond.appendChild(condVal);
-    receipt.appendChild(cond);
-
-    // Grab order total from Backend
-    let total = document.createElement("h2");
-    let totalVal = document.createTextNode("Total: ##WIP##");
-
-    total.appendChild(totalVal);
-    receipt.appendChild(total);
 }
 
 if (document.getElementById('getReceipt')) {
